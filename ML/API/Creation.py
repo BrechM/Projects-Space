59c4a14be2b82a8f43cfc276baf5348cc67e01f6
BrechM <brechmovilnganga@gmail.com>
2025-04-26 15:01:26 +0200

    mise à jour

--- ML/API/Creation.py
+++ ML/API/Creation.py
@@ -5,7 +5,28 @@ import random
 
 # On va creer l'appli fastApi
 app = FastAPI()
-@app.get("/")
+@app.get("/") # mon premier endpoint
 
 def accueil():
     return {"Message": "Bienvenu sur notre API de generation de nombre aleatoire"}
+
+# On va rajouter un endpoints, qui permet à acceder uniquement à une information
+@app.get("/nombre_aleatoire") # mon deuxieme endpoint
+def generer_nombre():
+    """
+        cette fonction permet de generer un nombre aleatoire entre 0 et 1000
+    """
+    return {"nombre_aleatoire": random.randint(0,1000)}
+
+# On peut modifier la fonction, mettre par exemple un min max
+# que l'utilisateur decide decide lui meme d'un min ou d'un max
+
+@app.get("/nombre_aleatoire/{min}_{max}") # mon deuxieme endpoint
+def generer_nombre(min: int=0, max: int =1000):
+    """
+        cette fonction permet de generer un nombre aleatoire entre 0 et 1000
+    """
+    return {"nombre_aleatoire": random.randint(min,max)}
+
+#### Pour tester l'API : uvicorn Creation:app --reload
+### pour voir la documentation, on rajoute un /docs
